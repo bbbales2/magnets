b142c571a19dba93f8be30a52c45d165967a1c50
Ben Bales <bbbales2@gmail.com>
2016-11-21 11:02:31 -0800

    Added units

--- interpsmooth.py
+++ interpsmooth.py
@@ -14,8 +14,9 @@ def split_MTs(df, col="Magnetic Field (T)", precision=3):
 def interpolate_data(df, N):
     M = {}
     Mhs = []
-    Ts = []
-        
+
+    hs = []
+
     temp_min = 1e6
     temp_max = 0
     for T, data in split_MTs(df):
@@ -26,9 +27,9 @@ def interpolate_data(df, N):
 
         temp_max = max(tmax, temp_max)
         temp_min = min(tmin, temp_min)
-    
-    temps = numpy.linspace(temp_min, temp_max, N)
-    # Read in the data 
+
+    ts = numpy.linspace(temp_min, temp_max, N)
+    # Read in the data
     for T, data in split_MTs(df):
         # Only read in the bottom half
         idx = numpy.argmin(data['Temperature (K)'].values)
@@ -43,28 +44,28 @@ def interpolate_data(df, N):
 
         temperature = numpy.array(temperature)[idxs]
         mags = numpy.array(mags)[idxs]
-        
+
         # 1D Interpolate in the direction of temperatures
-        magsi = numpy.interp(temps, temperature, mags)
+        magsi = numpy.interp(ts, temperature, mags)
         Mhs.append(magsi)
-        Ts.append(T)
+        hs.append(T)
 
         M[T] = numpy.array((temperature, mags))
-    
-    idxs = numpy.argsort(Ts)
-    Ts = numpy.array(Ts)[idxs]
+
+    idxs = numpy.argsort(hs)
+    hs = numpy.array(hs)[idxs]
     Mhs = numpy.array(Mhs)[idxs]
-    
+
     # Build a 2d interpolation of temps and magnetic fields
-    interp = scipy.interpolate.interp2d(temps, Ts, Mhs)
+    interp = scipy.interpolate.interp2d(ts, hs, Mhs)
 
-    Ts2 = numpy.linspace(min(Ts), max(Ts), N)
+    hs2 = numpy.linspace(min(hs), max(hs), N)
     #temps2 = numpy.linspace(min(temps), max(temps), N)
-    
-    # Evaluate interpolation at the data points
-    Mhis = interp(temps, Ts2)
 
-    return Mhis
+    # Evaluate interpolation at the data points
+    Mhis = interp(ts, hs2)
+
+    return ts, hs2, numpy.flipud(Mhis)
 
 # Grid size
 N = 50
@@ -83,11 +84,14 @@ dt = ys[1] - ys[0]
 
 # Read in data and build an interpolation
 df = pd.read_pickle(f)
-mh = numpy.flipud(interpolate_data(df, N))
+ts, hs, mh = interpolate_data(df, N)
+
+plt.imshow(mh, interpolation = 'NONE')
+plt.show()
 
 # Loss function friendly for scipy.minimize
 def f_loss(sm, mh = mh, N = N, dt = dt, dh = dh):
-    
+
     # Take stacked vector and reconstruct
     # s and m0
     st = sm.reshape((N+2,N))
@@ -107,7 +111,7 @@ def df_loss(sm, mh = mh, N = N, dt = dt, dh = dh):
     st = sm.reshape((N+2,N))
     m0 = st[N+1,:]
     sh = st[:N+1,:]
-    
+
     # Evaluate m, dl/ds, dl/dm0 and restack them
     m = smooth_funcs.getm(N, dt, dh, sh, m0)
     dlds = smooth_funcs.dlossds(alpha, N, dt, dh, m, mh, sh)
@@ -129,7 +133,7 @@ for i in range(N):
     bounds[N * N + i] = (0,0)
 
 res = minimize(f_loss, sm, method='L-BFGS-B', bounds = bounds, jac = df_loss, options={'disp':True})
-
+#%%
 # Reconstruct s and m0 from vector form
 approx_t = res.x.reshape((N+2,N))
 sapprox = approx_t[:N+1,:]
@@ -139,6 +143,22 @@ m0approx = approx_t[N+1,:]
 m = smooth_funcs.getm(N, dt, dh, sapprox, m0approx)
 
 # Plotting
+
+# rescale from [0.0, 1.0] x [0.0, 1.0] to physical units
+s = sapprox * ((xs[1] - xs[0]) / (ts[1] - ts[0])) * ((hs[1] - hs[0]) / (ys[1] - ys[0]))
+plt.imshow(s, interpolation = 'NONE', extent = [min(ts), max(ts), min(hs), max(hs)], aspect = (max(ts) - min(ts)) / (max(hs) - min(hs)))
+plt.xlabel('Temperature')
+plt.ylabel('Magnetic field')
+plt.title('$\Delta S_M$')
+plt.colorbar()
+fig = plt.gcf()
+fig.set_size_inches((10, 10))
+#xs = numpy.linspace(0.0, N, 6)
+#plt.xticks(xs, numpy.interp(xs, [0.0, N], [min(ts), max(ts)]))
+#plt.yticks(xs, numpy.interp(xs, [0.0, N], [min(hs), max(hs)]))
+plt.show()
+
+#%%
 plt.imshow(sapprox, interpolation = 'NONE')
 plt.title('s')
 plt.colorbar()
