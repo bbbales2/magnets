9002b1b860c5aaf5713acc1ec5e64bf55089b296
Ben Bales <bbbales2@gmail.com>
2017-01-07 11:29:54 -0800

    Changed the boundary conditions a little. Still not satisfied

--- variational_sreg.py
+++ variational_sreg.py
@@ -15,7 +15,7 @@ f.close()
 
 #m = numpy.flipud(m)
 
-m = m[::1, ::1]
+m = m[::4, ::4]
 
 plt.imshow(m)
 plt.colorbar()
@@ -38,7 +38,7 @@ dy = ys[1] - ys[0]
 #plt.show()
 #%%
 
-a = 0.001
+a = 0.0001
 
 A = numpy.zeros((m.shape[0], m.shape[1], m.shape[0], m.shape[1]))
 b = numpy.zeros(m.shape)
@@ -64,42 +64,50 @@ for i in range(I):
             A[i, j, i - 1, j] += 1 / dy**2
             A[i, j, i, j] -= 2 / dy**2
             A[i, j, i + 1, j] += 1 / dy**2
-                    
+
         #if i < I - 1:
-            
+
 #        if j > 0:
 #            A[i, j, i, j - 2] += -0.5 * a / dx**4
-#            
+#
 #        if j > 1:
 #            A[i, j, i, j - 1] -= 4 * -0.5 * a / dx**4
-#            
+#
 #        A[i, j, i, j] += -0.5 * 6 * a / dx**4
-#        
+#
 #        if j < J - 1:
 #            A[i, j, i, j + 1] -= 4 * -0.5 * a / dx**4
-#            
+#
 #        if j < J - 2:
 #            A[i, j, i, j + 2] += -0.5 * a / dx**4
-        
-        if j == 1:
-            A[i, j, i, j - 1] -= 0.5 * -0.5 * a / (2.0 * dx**4)
-            A[i, j, i, j] += 1.0 * -0.5 * a / (2.0 * dx**4)
-            A[i, j, i, j + 2] -= 1.0 * -0.5 * a / (2.0 * dx**4)
-            A[i, j, i, j + 3] += 0.5 * -0.5 * a / (2.0 * dx**4)
-        elif j == J - 2:
-            A[i, j, i, j - 3] += 0.5 * -0.5 * a / (2.0 * dx**4)
-            A[i, j, i, j - 2] -= 1.0 * -0.5 * a / (2.0 * dx**4)
-            A[i, j, i, j] += 1.0 * -0.5 * a / (2.0 * dx**4)
-            A[i, j, i, j + 1] -= 0.5 * -0.5 * a / (2.0 * dx**4)
-        elif j > 1 and j < J - 2:
+
+# I think this is correct but stuff works better with it commented
+#        if j == 1:
+#            A[i, j, i, j - 1] -= 2 * -0.5 * a / (dx**4)
+#            A[i, j, i, j] += 5.0 * -0.5 * a / (dx**4)
+#            A[i, j, i, j + 2] -= 4.0 * -0.5 * a / (dx**4)
+#            A[i, j, i, j + 3] += 1.0 * -0.5 * a / (dx**4)
+#        elif j == J - 2:
+#            A[i, j, i, j - 3] += 1.0 * -0.5 * a / (dx**4)
+#            A[i, j, i, j - 2] -= 4.0 * -0.5 * a / (dx**4)
+#            A[i, j, i, j] += 5.0 * -0.5 * a / (dx**4)
+#            A[i, j, i, j + 1] -= 2.0 * -0.5 * a / (dx**4)
+# I don't think this is correct
+#        if j == 1:
+#            A[i, j, i, j - 1] -= 0.5 * -0.5 * a / (2.0 * dx**4)
+#            A[i, j, i, j] += 1.0 * -0.5 * a / (2.0 * dx**4)
+#            A[i, j, i, j + 2] -= 1.0 * -0.5 * a / (2.0 * dx**4)
+#            A[i, j, i, j + 3] += 0.5 * -0.5 * a / (2.0 * dx**4)
+#        elif j == J - 2:
+#            A[i, j, i, j - 3] += 0.5 * -0.5 * a / (2.0 * dx**4)
+#            A[i, j, i, j - 2] -= 1.0 * -0.5 * a / (2.0 * dx**4)
+#            A[i, j, i, j] += 1.0 * -0.5 * a / (2.0 * dx**4)
+#            A[i, j, i, j + 1] -= 0.5 * -0.5 * a / (2.0 * dx**4)
+        if j > 1 and j < J - 2:
             A[i, j, i, j - 2] += -0.5 * a / dx**4
-            
             A[i, j, i, j - 1] -= 4 * -0.5 * a / dx**4
-            
             A[i, j, i, j] += -0.5 * 6 * a / dx**4
-
             A[i, j, i, j + 1] -= 4 * -0.5 * a / dx**4
-
             A[i, j, i, j + 2] += -0.5 * a / dx**4
 #
 #        if i > 0:
@@ -108,61 +116,61 @@ for i in range(I):
 #                A[i, j, i - 1, j] -= -a / (dy**2 * dx**2)
 #                A[i, j, i, j - 1] -= -a / (dy**2 * dx**2)
 #                A[i, j, i, j] += -a / (dy**2 * dx**2)
-#                
+#
 #        if i > 0:
 #            if j < J - 1:
 #                A[i, j, i - 1, j] += -a / (dy**2 * dx**2)
 #                A[i, j, i - 1, j + 1] -= -a / (dy**2 * dx**2)
 #                A[i, j, i, j] -= -a / (dy**2 * dx**2)
 #                A[i, j, i, j + 1] += -a / (dy**2 * dx**2)
-#                
+#
 #        if i < I - 1:
 #            if j > 0:
 #                A[i, j, i, j - 1] += -a / (dy**2 * dx**2)
 #                A[i, j, i, j] -= -a / (dy**2 * dx**2)
 #                A[i, j, i + 1, j - 1] -= -a / (dy**2 * dx**2)
 #                A[i, j, i + 1, j] += -a / (dy**2 * dx**2)
-#                
+#
 #        if i < I - 1:
 #            if j < J - 1:
 #                A[i, j, i, j] += -a / (dy**2 * dx**2)
 #                A[i, j, i, j + 1] -= -a / (dy**2 * dx**2)
 #                A[i, j, i + 1, j] -= -a / (dy**2 * dx**2)
 #                A[i, j, i + 1, j + 1] += -a / (dy**2 * dx**2)
-            
+
 
         if j == 0 or j == J - 1:
             pass
         elif i == 0:
             A[i, j, i, j - 1] -= 1 * -a / (dy**2 * dx**2)
-                
+
             A[i, j, i, j] += 2 * -a / (dy**2 * dx**2)
-                
+
             A[i, j, i, j + 1] -= 1 * -a / (dy**2 * dx**2)
-                
+
             A[i, j, i + 1, j - 1] += -a / (dy**2 * dx**2)
-                
+
             A[i, j, i + 1, j] -= 2 * -a / (dy**2 * dx**2)
-                
+
             A[i, j, i + 1, j + 1] += -a / (dy**2 * dx**2)
-                
-                
-                
+
+
+
 #        elif i == I - 1:
 #            if i > 0 and j > 0:
 #                A[i, j, i - 1, j - 1] += -a / (dy**2 * dx**2)
-#                
+#
 #            if i > 0:
 #                A[i, j, i - 1, j] -= 2 * -a / (dy**2 * dx**2)
-#                
+#
 #            if i > 0 and j < J - 1:
 #                A[i, j, i - 1, j + 1] += -a / (dy**2 * dx**2)
-#                
+#
 #            if j > 0:
 #                A[i, j, i, j - 1] -= 1 * -a / (dy**2 * dx**2)
-#                
+#
 #            A[i, j, i, j] += 2 * -a / (dy**2 * dx**2)
-#                
+#
 #            if j < J - 1:
 #                A[i, j, i, j + 1] -= 1 * -a / (dy**2 * dx**2)
 
@@ -170,27 +178,27 @@ for i in range(I):
         else:
             if i > 0 and j > 0:
                 A[i, j, i - 1, j - 1] += -a / (dy**2 * dx**2)
-                
+
             if i > 0:
                 A[i, j, i - 1, j] -= 2 * -a / (dy**2 * dx**2)
-                
+
             if i > 0 and j < J - 1:
                 A[i, j, i - 1, j + 1] += -a / (dy**2 * dx**2)
-                
+
             if j > 0:
                 A[i, j, i, j - 1] -= 2 * -a / (dy**2 * dx**2)
-                
+
             A[i, j, i, j] += 4 * -a / (dy**2 * dx**2)
-            
+
             if j < J - 1:
                 A[i, j, i, j + 1] -= 2 * -a / (dy**2 * dx**2)
-                
+
             if i < I - 1 and j > 0:
                 A[i, j, i + 1, j - 1] += -a / (dy**2 * dx**2)
-                
+
             if i < I - 1:
                 A[i, j, i + 1, j] -= 2 * -a / (dy**2 * dx**2)
-                
+
             if i < I - 1 and j < J - 1:
                 A[i, j, i + 1, j + 1] += -a / (dy**2 * dx**2)
 
